db1caa3d6ebd72dec37007832447a9cdbf533a76
Kayky Freitas <kayky_defreitas@hotmail.com>
2023-10-26 15:12:11 -0300

    feat: (infrastructure) Adding database config

--- infrastructure/src/test/java/dev/kaykyfreitas/video/catalog/admin/infrastructure/MainTest.java
+++ infrastructure/src/test/java/dev/kaykyfreitas/video/catalog/admin/infrastructure/MainTest.java
@@ -1,12 +1,12 @@
 package dev.kaykyfreitas.video.catalog.admin.infrastructure;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
     @Test
     public void testMain() {
-        Assertions.assertNotNull(new Main());
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Main.main(new String[]{});
     }
 }
