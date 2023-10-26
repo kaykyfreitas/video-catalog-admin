e43b5d1095a6520db9fbfb106ebb14c2e208beb5
Kayky Freitas <kayky_defreitas@hotmail.com>
2023-10-26 13:54:27 -0300

    feat: (infrastructure) Adding Spring Boot

---
+++ infrastructure/src/main/java/dev/kaykyfreitas/video/catalog/admin/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package dev.kaykyfreitas.video.catalog.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("dev.kaykyfreitas.video.catalog.admin")
+public class WebServerConfig {
+}
