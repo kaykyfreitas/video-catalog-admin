940423292aba5278f8bc54d54599762c5905a1d7
Kayky Freitas <kayky_defreitas@hotmail.com>
2023-10-26 13:12:21 -0300

    feat: (application) ListCategoriesUseCase

---
+++ application/src/main/java/dev/kaykyfreitas/video/catalog/admin/application/category/retrieve/list/DefaultListCategoriesUseCase.java
@@ -0,0 +1,22 @@
+package dev.kaykyfreitas.video.catalog.admin.application.category.retrieve.list;
+
+import dev.kaykyfreitas.video.catalog.admin.domain.category.CategoryGateway;
+import dev.kaykyfreitas.video.catalog.admin.domain.category.CategorySearchQuery;
+import dev.kaykyfreitas.video.catalog.admin.domain.pagination.Pagination;
+
+import java.util.Objects;
+
+public class DefaultListCategoriesUseCase extends ListCategoriesUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultListCategoriesUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public Pagination<CategoryListOutput> execute(final CategorySearchQuery aQuery) {
+        return this.categoryGateway.findAll(aQuery)
+                .map(CategoryListOutput::from);
+    }
+}
