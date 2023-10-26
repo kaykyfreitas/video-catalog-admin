be7069e50477804c622dea2548d698ab89d306d9
Kayky Freitas <kayky_defreitas@hotmail.com>
2023-10-26 13:11:50 -0300

    feat: (domain) Adding pagination map

--- domain/src/main/java/dev/kaykyfreitas/video/catalog/admin/domain/pagination/Pagination.java
+++ domain/src/main/java/dev/kaykyfreitas/video/catalog/admin/domain/pagination/Pagination.java
@@ -1,6 +1,7 @@
 package dev.kaykyfreitas.video.catalog.admin.domain.pagination;
 
 import java.util.List;
+import java.util.function.Function;
 
 public record Pagination<T>(
         int currentPage,
@@ -8,4 +9,10 @@ public record Pagination<T>(
         long total,
         List<T> items
 ) {
+    public <R> Pagination<R> map(final Function<T, R> mapper) {
+        final var aNewList = this.items.stream()
+                .map(mapper)
+                .toList();
+        return new Pagination<>(currentPage, perPage, total, aNewList);
+    }
 }
