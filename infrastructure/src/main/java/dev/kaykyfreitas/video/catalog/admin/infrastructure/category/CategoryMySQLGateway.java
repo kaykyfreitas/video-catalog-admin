eec323ea34887346b67abe3928f92ae3e2d226c6
Kayky Freitas <kayky_defreitas@hotmail.com>
2023-10-31 16:21:39 -0300

    feat: (infrastructure) Category findById

--- infrastructure/src/main/java/dev/kaykyfreitas/video/catalog/admin/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/dev/kaykyfreitas/video/catalog/admin/infrastructure/category/CategoryMySQLGateway.java
@@ -34,7 +34,8 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Optional<Category> findById(CategoryId anId) {
-        return Optional.empty();
+        return this.repository.findById(anId.getValue())
+                .map(CategoryJpaEntity::toAggreagate);
     }
 
     @Override
