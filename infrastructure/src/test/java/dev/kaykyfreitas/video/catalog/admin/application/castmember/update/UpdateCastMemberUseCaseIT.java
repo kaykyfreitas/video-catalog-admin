bd92483d9f94432de01440948255f8a7282b3044
Kayky Freitas <kayky_defreitas@hotmail.com>
2024-03-02 21:40:27 -0300

    test: (infrastructure) Update CastMember use case integration test

---
+++ infrastructure/src/test/java/dev/kaykyfreitas/video/catalog/admin/application/castmember/update/UpdateCastMemberUseCaseIT.java
@@ -0,0 +1,174 @@
+package dev.kaykyfreitas.video.catalog.admin.application.castmember.update;
+
+import dev.kaykyfreitas.video.catalog.admin.Fixture;
+import dev.kaykyfreitas.video.catalog.admin.IntegrationTest;
+import dev.kaykyfreitas.video.catalog.admin.domain.castmember.CastMember;
+import dev.kaykyfreitas.video.catalog.admin.domain.castmember.CastMemberGateway;
+import dev.kaykyfreitas.video.catalog.admin.domain.castmember.CastMemberId;
+import dev.kaykyfreitas.video.catalog.admin.domain.castmember.CastMemberType;
+import dev.kaykyfreitas.video.catalog.admin.domain.exceptions.NotFoundException;
+import dev.kaykyfreitas.video.catalog.admin.domain.exceptions.NotificationException;
+import dev.kaykyfreitas.video.catalog.admin.infrastructure.castmember.persistence.CastMemberJpaEntity;
+import dev.kaykyfreitas.video.catalog.admin.infrastructure.castmember.persistence.CastMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@IntegrationTest
+public class UpdateCastMemberUseCaseIT {
+
+    @Autowired
+    private DefaultUpdateCastMemberUseCase useCase;
+
+    @SpyBean
+    private CastMemberGateway castMemberGateway;
+
+    @Autowired
+    private CastMemberRepository castMemberRepository;
+
+    @Test
+    public void givenAValidCommand_whenCallsUpdateCastMember_shouldReturnItsIdentifier() {
+        // given
+        final var aMember = CastMember.newMember("vin diesel", CastMemberType.DIRECTOR);
+
+        this.castMemberRepository.saveAndFlush(CastMemberJpaEntity.from(aMember));
+        Assertions.assertEquals(1, castMemberRepository.count());
+
+        final var expectedId = aMember.getId();
+        final var expectedName = Fixture.name();
+        final var expectedType = CastMemberType.ACTOR;
+
+        final var aCommand = UpdateCastMemberCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedType
+        );
+
+        // when
+        final var actualOutput = useCase.execute(aCommand);
+
+        // then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertEquals(expectedId.getValue(), actualOutput.id());
+
+        final var actualPersistedMember = this.castMemberRepository.findById(expectedId.getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualPersistedMember.getName());
+        Assertions.assertEquals(expectedType, actualPersistedMember.getType());
+        Assertions.assertEquals(aMember.getCreatedAt(), actualPersistedMember.getCreatedAt());
+        Assertions.assertTrue(aMember.getUpdatedAt().isBefore(actualPersistedMember.getUpdatedAt()));
+
+        verify(castMemberGateway).findById(any());
+
+        verify(castMemberGateway).update(any());
+    }
+
+    @Test
+    public void givenAnInvalidName_whenCallsUpdateCastMember_shouldThrowsNotificationException() {
+        // given
+        final var aMember = CastMember.newMember("vin diesel", CastMemberType.DIRECTOR);
+
+        this.castMemberRepository.saveAndFlush(CastMemberJpaEntity.from(aMember));
+        Assertions.assertEquals(1, this.castMemberRepository.count());
+
+        final var expectedId = aMember.getId();
+        final String expectedName = null;
+        final var expectedType = CastMemberType.ACTOR;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aCommand = UpdateCastMemberCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedType
+        );
+
+        // when
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> useCase.execute(aCommand)
+        );
+
+        // then
+        Assertions.assertNotNull(actualException);
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(castMemberGateway).findById(any());
+        verify(castMemberGateway, times(0)).update(any());
+    }
+
+    @Test
+    public void givenAnInvalidType_whenCallsUpdateCastMember_shouldThrowsNotificationException() {
+        // given
+        final var aMember = CastMember.newMember("vin diesel", CastMemberType.DIRECTOR);
+
+        this.castMemberRepository.saveAndFlush(CastMemberJpaEntity.from(aMember));
+        Assertions.assertEquals(1, this.castMemberRepository.count());
+
+        final var expectedId = aMember.getId();
+        final var expectedName = Fixture.name();
+        final CastMemberType expectedType = null;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var aCommand = UpdateCastMemberCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedType
+        );
+
+        // when
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> useCase.execute(aCommand)
+        );
+
+        // then
+        Assertions.assertNotNull(actualException);
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(castMemberGateway).findById(any());
+        verify(castMemberGateway, times(0)).update(any());
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsUpdateCastMember_shouldThrowsNotFoundException() {
+        // given
+        final var expectedId = CastMemberId.from("123");
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var expectedErrorMessage = "castmember with id 123 was not found";
+
+        final var aCommand = UpdateCastMemberCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedType
+        );
+
+        // when
+        final var actualException = Assertions.assertThrows(
+                NotFoundException.class,
+                () -> useCase.execute(aCommand)
+        );
+
+        // then
+        Assertions.assertNotNull(actualException);
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        verify(castMemberGateway).findById(any());
+        verify(castMemberGateway, times(0)).update(any());
+    }
+}
