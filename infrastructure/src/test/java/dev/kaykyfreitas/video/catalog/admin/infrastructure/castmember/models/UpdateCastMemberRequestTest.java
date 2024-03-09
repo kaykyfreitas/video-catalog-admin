99108c63b35b00b7d3a01c565336b69a672fae00
Kayky Freitas <kayky_defreitas@hotmail.com>
2024-03-09 01:34:11 -0300

    test: (infrastructure) CastMember marshall and unmarshall tests

---
+++ infrastructure/src/test/java/dev/kaykyfreitas/video/catalog/admin/infrastructure/castmember/models/UpdateCastMemberRequestTest.java
@@ -0,0 +1,35 @@
+package dev.kaykyfreitas.video.catalog.admin.infrastructure.castmember.models;
+
+import dev.kaykyfreitas.video.catalog.admin.Fixture;
+import dev.kaykyfreitas.video.catalog.admin.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+public class UpdateCastMemberRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCastMemberRequest> json;
+
+    @Test
+    public void testUnmarshall() throws Exception {
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var json = """
+                {
+                  "name": "%s",
+                  "type": "%s"
+                }
+                """.formatted(expectedName, expectedType);
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("type", expectedType);
+    }
+
+}
